39405471e93b636a057d13d8b8f7cd34afa8e051
Samuel Cristobal <el.tobal@gmail.com>
2023-03-26 09:32:02 +0200

    simplified commands

--- src/telegram_bot.rs
+++ src/telegram_bot.rs
@@ -1,8 +1,7 @@
-use std::fmt::Display;
-
 use crate::openai_client::{self, reply};
-
+use dptree::case;
 use serde::Serialize;
+use std::fmt::Display;
 use teloxide::{
     dispatching::{
         dialogue::{self, InMemStorage},
@@ -16,8 +15,6 @@ use teloxide::{
 use tracing::{error, instrument};
 use uuid::Uuid;
 
-use dptree::case;
-
 #[derive(BotCommands, Clone)]
 #[command(
     rename_rule = "lowercase",
@@ -25,21 +22,13 @@ use dptree::case;
 )]
 #[derive(Debug)]
 pub enum Command {
-    #[command(description = "Give the current state of the bot")]
-    State,
-    #[command(description = "Clear chat history")]
-    Reset,
-    #[command(description = "The bot will stop listening to messages")]
-    Mute,
-    #[command(description = "The bot will start listening to messagess")]
-    Listen,
-    #[command(description = "Make a summary of the group activity")]
-    Summarize,
-    #[command(description = "Chat with the bot")]
+    #[command(description = "Keep the conversation going, the bot will keep context until /reset")]
     Chat { text: String },
+    #[command(description = "Ask questions in the context of the group conversation")]
+    Group,
+    #[command(description = "Wipe chat from the bot's memory")]
+    Reset,
 }
-
-type GroupHistory = Vec<Message>;
 
 #[derive(Debug, Clone, Copy, Serialize)]
 pub enum Role {
@@ -57,6 +46,8 @@ pub struct BotMessage {
 }
 
 type BotHistory = Vec<BotMessage>;
+
+type GroupHistory = Vec<Message>;
 
 #[derive(Debug, Clone, Default)]
 pub struct History {
@@ -87,22 +78,18 @@ impl Display for State {
 
 #[instrument]
 pub fn schema() -> UpdateHandler<anyhow::Error> {
-    let cmd_handler = filter_command::<Command, _>()
-        .branch(case![Command::State].endpoint(state))
-        .branch(case![State::Offline].branch(case![Command::Listen].endpoint(listen)))
-        .branch(
-            case![State::Online(msgs)]
-                .branch(case![Command::Summarize].endpoint(sumarize))
-                .branch(case![Command::Reset].endpoint(reset))
-                .branch(case![Command::Mute].endpoint(mute))
-                .branch(case![Command::Chat { text }].endpoint(chat)),
-        );
+    let cmd_handler = filter_command::<Command, _>().branch(
+        case![State::Online(msgs)]
+            .branch(case![Command::Group].endpoint(group))
+            .branch(case![Command::Reset].endpoint(reset))
+            .branch(case![Command::Chat { text }].endpoint(chat)),
+    );
 
     let msg_handler = Update::filter_message()
+        .branch(case![State::Offline].endpoint(do_nothing))
         .branch(cmd_handler)
-        .branch(case![State::Offline].endpoint(muted))
-        .branch(case![State::Online(msgs)].endpoint(incoming_message))
-        .endpoint(invalid);
+        .branch(case![State::Online(msgs)].endpoint(record))
+        .endpoint(do_nothing);
 
     dialogue::enter::<Update, InMemStorage<State>, State, _>().branch(msg_handler)
 }
@@ -111,75 +98,8 @@ type InMemDialogue = Dialogue<State, InMemStorage<State>>;
 
 type HandlerResult = Result<(), anyhow::Error>;
 
-async fn state(bot: Bot, dialogue: InMemDialogue, message: Message) -> HandlerResult {
-    let state = dialogue.get().await?;
-
-    let reply_txt = match state {
-        None => "No active conversation".to_string(),
-        Some(state) => format!("{}", state),
-    };
-
-    bot.send_message(message.chat.id, reply_txt)
-        .parse_mode(ParseMode::MarkdownV2)
-        .await?;
-
-    Ok(())
-}
-
-async fn listen(bot: Bot, dialogue: InMemDialogue, message: Message) -> HandlerResult {
-    dialogue.update(State::default()).await?;
-
-    bot.send_message(message.chat.id, State::default().to_string())
-        .parse_mode(ParseMode::MarkdownV2)
-        .await?;
-
-    Ok(())
-}
-
-async fn mute(bot: Bot, dialogue: InMemDialogue, message: Message) -> HandlerResult {
-    dialogue.update(State::Offline).await?;
-
-    bot.send_message(message.chat.id, State::Offline.to_string())
-        .parse_mode(ParseMode::MarkdownV2)
-        .await?;
-
-    Ok(())
-}
-
-async fn invalid(bot: Bot, dialogue: InMemDialogue, message: Message) -> HandlerResult {
-    dialogue.exit().await?;
-
-    let error_id = Uuid::new_v4().simple().to_string();
-
-    error!(error_id);
-
-    bot.send_message(
-                message.chat.id,
-                format!("there was an error processing your request, you can use this ID to track the issue `{}`", error_id),
-            ).parse_mode(ParseMode::MarkdownV2)
-            .await?;
-
-    Ok(())
-}
-
-async fn sumarize(bot: Bot, dialogue: InMemDialogue, message: Message) -> HandlerResult {
-    let state = dialogue.get().await?;
-
-    let messages = match state {
-        None => {
-            bot.send_message(message.chat.id, "No active conversation")
-                .await?;
-            return Ok(());
-        }
-        Some(State::Offline) => {
-            bot.send_message(message.chat.id, "The bot is offline")
-                .await?;
-            return Ok(());
-        }
-        Some(State::Online(msgs)) => msgs,
-    };
-
-    let openai_response = openai_client::sumarize(&messages.group_history).await;
+async fn group(bot: Bot, message: Message, history: History) -> HandlerResult {
+    let openai_response = openai_client::sumarize(&history.group_history).await;
 
     match openai_response {
         Err(e) => {
@@ -211,29 +131,35 @@ async fn sumarize(bot: Bot, dialogue: InMemDialogue, message: Message) -> Handle
     Ok(())
 }
 
-async fn reset(bot: Bot, dialogue: InMemDialogue, message: Message) -> HandlerResult {
-    dialogue.update(State::default()).await?;
+async fn reset(
+    bot: Bot,
+    dialogue: InMemDialogue,
+    message: Message,
+    mut history: History,
+) -> HandlerResult {
+    history.bot_history = BotHistory::default();
 
-    bot.send_message(message.chat.id, State::default().to_string())
+    dialogue.update(State::Online(history)).await?;
+
+    bot.send_message(message.chat.id, "`Bot chat history has been erased` ✅")
         .parse_mode(ParseMode::MarkdownV2)
         .await?;
 
     Ok(())
 }
 
-async fn muted() -> HandlerResult {
+async fn do_nothing() -> HandlerResult {
     // if the bot is muted do nothing
     Ok(())
 }
 
-async fn incoming_message(
+async fn record(
     dialogue: InMemDialogue,
     new_message: Message,
     mut history: History,
 ) -> HandlerResult {
     history.group_history.push(new_message);
     dialogue.update(State::Online(history)).await?;
-
     Ok(())
 }
 
