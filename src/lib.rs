782ef2f9656ab1fbdb8126e79b817bd9e3076538
Samuel Cristobal <el.tobal@gmail.com>
2023-03-26 10:28:53 +0200

    removed adaptors to keep clients self contained

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-pub mod adaptors;
 pub mod health_checker;
 pub mod openai_client;
 pub mod telegram_bot;
--- src/openai_client.rs
+++ src/openai_client.rs
@@ -1,12 +1,15 @@
 use async_openai::{
     error::OpenAIError,
     types::{
-        ChatCompletionRequestMessage, CreateChatCompletionRequestArgs, CreateChatCompletionResponse,
+        ChatCompletionRequestMessage, ChatCompletionRequestMessageArgs,
+        CreateChatCompletionRequestArgs, CreateChatCompletionResponse,
     },
     Client,
 };
 use teloxide::types::Message;
 use tracing::instrument;
+
+use crate::telegram_bot;
 
 #[instrument]
 pub async fn group_question(
@@ -51,6 +54,30 @@ pub async fn group_question(
         .build()?;
 
     client.chat().create(request).await
+}
+
+impl From<telegram_bot::Role> for async_openai::types::Role {
+    fn from(val: telegram_bot::Role) -> Self {
+        match val {
+            telegram_bot::Role::System => async_openai::types::Role::System,
+            telegram_bot::Role::User => async_openai::types::Role::User,
+            telegram_bot::Role::Assistant => async_openai::types::Role::Assistant,
+        }
+    }
+}
+
+impl From<telegram_bot::BotMessage> for async_openai::types::ChatCompletionRequestMessage {
+    fn from(value: telegram_bot::BotMessage) -> Self {
+        let mut req = ChatCompletionRequestMessageArgs::default();
+
+        req.role(value.role).content(&value.content);
+
+        if let Some(name) = &value.name {
+            req.name(name);
+        }
+
+        req.build().unwrap()
+    }
 }
 
 #[instrument]
