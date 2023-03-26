bad11c28493d6e2a90ff6be476caf2f0325a1a7d
Samuel Cristobal <el.tobal@gmail.com>
2023-03-26 19:33:40 +0200

    fixed API types

--- src/replicate_client.rs
+++ src/replicate_client.rs
@@ -4,21 +4,21 @@ use reqwest::{
 };
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
-use tracing::{info, instrument};
+use tracing::info;
 
 const MODEL_URL: &str = "https://api.replicate.com/v1/predictions";
 
 #[derive(Deserialize, Debug, Clone)]
-pub struct Response<Input, Output> {
+pub struct ReplicateResponse<ModelInput, ModelOutput> {
     completed_at: Option<String>,
     created_at: Option<String>,
     error: Option<String>,
     hardware: Option<String>,
     id: String,
-    input: Input,
-    logs: String,
-    metrics: Metrics,
-    output: Output,
+    input: ModelInput,
+    logs: Option<String>,
+    metrics: Option<Metrics>,
+    output: Option<ModelOutput>,
     started_at: Option<String>,
     status: String,
     urls: Urls,
@@ -26,9 +26,10 @@ pub struct Response<Input, Output> {
     webhook_completed: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Deserialize, Debug, Clone)]
 struct Metrics {
-    predict_time: f32,
+    predict_time: Option<f32>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -39,68 +40,70 @@ struct Urls {
 
 #[skip_serializing_none]
 #[derive(Serialize, Debug)]
-struct Request<I> {
+struct ReplicateRequest<I> {
     version: String,
     input: I,
-    webhook_completed: Option<String>,
 }
 
 const MODEL_VERSION: &str = "328bd9692d29d6781034e3acab8cf3fcb122161e6f5afb896a4ca9fd57090577";
 
 #[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Input {
+pub struct StableDiffusionInput {
     prompt: String,
     seed: Option<u32>,
     num_inference_steps: Option<u32>,
     guidance_scale: Option<f32>,
 }
 
-type Output = Option<Vec<String>>;
+type StableDiffusionOutput = Option<Vec<String>>;
 
-async fn api_call<Request: Serialize>(
-    request: &Request,
-) -> Result<reqwest::Response, reqwest::Error> {
+type StableDiffusionRequest = ReplicateRequest<StableDiffusionInput>;
+type StableDiffusionResponse = ReplicateResponse<StableDiffusionInput, StableDiffusionOutput>;
+
+async fn api_call(
+    request: &StableDiffusionRequest,
+) -> Result<StableDiffusionResponse, reqwest::Error> {
     let client = Client::new();
 
     let token = std::env::var("REPLICATE_TOKEN").expect("REPLICATE_TOKEN must be set");
 
-    let body = serde_json::to_string(&request).unwrap();
-
-    client
+    let response = client
         .post(MODEL_URL.to_string())
         .header(CONTENT_TYPE, "application/json")
         .header(AUTHORIZATION, "Token ".to_string() + &token)
-        .body(body)
+        .json(request)
         .send()
-        .await
+        .await?;
+
+    info!(?response);
+
+    let response_body = response.json().await;
+
+    info!(?response_body);
+    response_body
 }
 
-#[instrument]
-pub async fn draw(prompt: String) -> Response<Input, Output> {
-    let request = Request {
+pub async fn draw(prompt: String) -> Result<StableDiffusionResponse, anyhow::Error> {
+    let input = StableDiffusionInput {
+        prompt,
+        seed: None,
+        num_inference_steps: None,
+        guidance_scale: None,
+    };
+
+    info!(?input);
+
+    let request = StableDiffusionRequest {
         version: MODEL_VERSION.to_string(),
-        input: Input {
-            prompt,
-            seed: None,
-            num_inference_steps: None,
-            guidance_scale: None,
-        },
-        webhook_completed: None,
+        input,
     };
 
     info!(?request);
 
-    let api_response = api_call(&request).await.unwrap();
-
-    info!(?api_response);
-
-    let response = api_response
-        .json::<Response<Input, Output>>()
-        .await
-        .unwrap();
+    let response = api_call(&request).await?;
 
     info!(?response);
 
-    response
+    Ok(response)
 }
